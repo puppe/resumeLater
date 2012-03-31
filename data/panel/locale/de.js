05d0fc649c90b07be7d585d0ca983327b1f2fce1
Martin Puppe <mail@mpuppe.de>
2012-03-31 02:54:37 +0200

    #6 German locale

---
+++ data/panel/locale/de.js
@@ -0,0 +1,7 @@
+var locale = {
+	'remove?' : 'Dieses Video entfernen?',
+	'Okay' : 'Okay',
+	'Cancel' : 'Abbrechen',
+	'private browsing' : 'Im privaten Modus können keine Videos gespeichert werden.',
+	'not supported' : 'Dieser Anbieter wird nicht unterstützt!'
+};
