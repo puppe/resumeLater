b677225e74e3788d7132a653ee4098d3928b40cf
Martin Puppe <mp@mpuppe.de>
2017-10-11 19:44:00 +0200

    Remove sample content script from webextension

--- webextension/background.js
+++ webextension/background.js
@@ -12,17 +12,3 @@ port.onMessage.addListener((msg) => {
         });
     }
 });
-
-browser.runtime.onMessage.addListener(msg => {
-    const {type} = msg;
-
-    switch (type) {
-    case "notify-attached-tab":
-        browser.notifications.create({
-            type: "basic",
-            title: "Attached to tab",
-            message: msg.message
-        });
-        break;
-    }
-});
