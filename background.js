0ac5d9706dcffe5221dc98938f84ee11a8f54957
Martin Puppe <mp@mpuppe.de>
2017-10-16 17:03:30 +0200

    Cleanup background.js
    The logic that was necessary for talking to an embedding SDK add-on, is
    not necessary anymore.

--- background.js
+++ background.js
@@ -1,16 +1,2 @@
 /* globals browser, console */
 "use strict";
-
-// Ask to the legacy part to dump the needed data and send it back
-// to the background page...
-var port = browser.runtime.connect({name: "sync-legacy-addon-data"});
-port.onMessage.addListener((msg) => {
-    if (msg) {
-        // Where it can be saved using the WebExtensions storage API.
-        browser.storage.local.set({
-            'legacy': msg,
-        });
-        console.debug('Received data from legacy add-on:');
-        console.debug(msg);
-    }
-});
