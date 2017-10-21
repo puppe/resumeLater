d55ad6bb023d4298f2857e405a9bdc70011b2967
Martin Puppe <mp@mpuppe.de>
2017-10-21 16:41:54 +0200

    Do not pollute global namespace
    This change is necessary because with webextensions librariers are
    loaded differently than before.

--- videos.js
+++ videos.js
@@ -1,5 +1,5 @@
 /*
-Copyright © 2012-2016 Martin Puppe
+Copyright © 2012-2017 Martin Puppe
 
 This file is part of resumeLater.
 
@@ -17,103 +17,105 @@ You should have received a copy of the GNU General Public License
 along with resumeLater. If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*global exports*/
-'use strict';
+let videos = (function () {
+    'use strict';
 
-const SCHEMA_VERSION = 1;
+    const SCHEMA_VERSION = 1;
 
-function getSiteName(video) {
-    var vid = video.vid;
-    return vid.slice(0, vid.indexOf("_"));
-}
-
-function getId(video) {
-    var vid = video.vid;
-    return vid.slice(vid.indexOf("_") + 1, vid.length);
-}
-
-var upgradeFunctions = [];
-
-upgradeFunctions[0] = function upgradeFrom0(storage) {
-    for (var vid in storage.videos) {
-        if (typeof storage.videos[vid] === 'undefined') {
-            delete storage.videos[vid];
-        } else if (!('lastModified' in storage.videos[vid])) {
-            storage.videos[vid].lastModified = storage.videos[vid].timeAdded || 0;
-            delete storage.videos[vid].timeAdded;
-        }
-    }
-};
-
-function VideoStorage(storage, optional) {
-    optional = optional || {};
-    var { simplePrefs, onUpdate } = optional;
-
-    onUpdate = onUpdate || function(_) {};
-
-    // upgrade storage to current schema version
-    if ('videos' in storage) {
-        storage.schemaVersion = storage.schemaVersion || 0;
-        while (storage.schemaVersion < SCHEMA_VERSION) {
-            upgradeFunctions[storage.schemaVersion](storage);
-            storage.schemaVersion++;
-        }
-    } else {
-        storage.videos = {};
-        storage.schemaVersion = SCHEMA_VERSION;
+    function getSiteName(video) {
+        var vid = video.vid;
+        return vid.slice(0, vid.indexOf("_"));
     }
 
-    this.add = function add(video) {
-        var properties = ['vid', 'title', 'time', 'playlistId'];
-        var copy = {};
+    function getId(video) {
+        var vid = video.vid;
+        return vid.slice(vid.indexOf("_") + 1, vid.length);
+    }
 
-        properties.forEach(function (prop) {
-            if (prop in video) {
-                copy[prop] = video[prop];
+    var upgradeFunctions = [];
+
+    upgradeFunctions[0] = function upgradeFrom0(storage) {
+        for (var vid in storage.videos) {
+            if (typeof storage.videos[vid] === 'undefined') {
+                delete storage.videos[vid];
+            } else if (!('lastModified' in storage.videos[vid])) {
+                storage.videos[vid].lastModified = storage.videos[vid].timeAdded || 0;
+                delete storage.videos[vid].timeAdded;
             }
-        });
-        copy.lastModified = new Date().getTime();
+        }
+    };
 
-        // optionally only keep one video per playlist
-        if (copy.playlistId &&
-        simplePrefs &&
-        simplePrefs.prefs.oneVideoPerPlaylist) {
-            for (var vid in storage.videos) {
-                if (storage.videos.hasOwnProperty(vid) &&
-                storage.videos[vid].playlistId === copy.playlistId) {
-                    this.remove(vid, true);
+    function VideoStorage(storage, optional) {
+        optional = optional || {};
+        var { simplePrefs, onUpdate } = optional;
+
+        onUpdate = onUpdate || function(_) {};
+
+        // upgrade storage to current schema version
+        if ('videos' in storage) {
+            storage.schemaVersion = storage.schemaVersion || 0;
+            while (storage.schemaVersion < SCHEMA_VERSION) {
+                upgradeFunctions[storage.schemaVersion](storage);
+                storage.schemaVersion++;
+            }
+        } else {
+            storage.videos = {};
+            storage.schemaVersion = SCHEMA_VERSION;
+        }
+
+        this.add = function add(video) {
+            var properties = ['vid', 'title', 'time', 'playlistId'];
+            var copy = {};
+
+            properties.forEach(function (prop) {
+                if (prop in video) {
+                    copy[prop] = video[prop];
+                }
+            });
+            copy.lastModified = new Date().getTime();
+
+            // optionally only keep one video per playlist
+            if (copy.playlistId &&
+                simplePrefs &&
+                simplePrefs.prefs.oneVideoPerPlaylist) {
+                for (var vid in storage.videos) {
+                    if (storage.videos.hasOwnProperty(vid) &&
+                        storage.videos[vid].playlistId === copy.playlistId) {
+                        this.remove(vid, true);
+                    }
                 }
             }
-        }
 
-        storage.videos[copy.vid] = copy;
-        onUpdate(this);
-    };
-
-    this.remove = function remove(vid, silent) {
-        delete storage.videos[vid];
-        if (!silent) {
+            storage.videos[copy.vid] = copy;
             onUpdate(this);
-        }
-    };
+        };
 
-    this.get = function get(vid) {
-        return storage.videos[vid];
-    };
-
-    this.getAll = function getAll() {
-        var array = [];
-        for (var vid in storage.videos) {
-            if (storage.videos.hasOwnProperty(vid)) {
-                array.push(storage.videos[vid]);
+        this.remove = function remove(vid, silent) {
+            delete storage.videos[vid];
+            if (!silent) {
+                onUpdate(this);
             }
-        }
-        return array;
-    };
-}
+        };
 
-exports.getSiteName = getSiteName;
-exports.getId = getId;
-exports.VideoStorage = VideoStorage;
-exports.SCHEMA_VERSION = SCHEMA_VERSION;
-// vim: set ts=4 sw=4 sts=4 tw=72 et :
+        this.get = function get(vid) {
+            return storage.videos[vid];
+        };
+
+        this.getAll = function getAll() {
+            var array = [];
+            for (var vid in storage.videos) {
+                if (storage.videos.hasOwnProperty(vid)) {
+                    array.push(storage.videos[vid]);
+                }
+            }
+            return array;
+        };
+    }
+
+    return {
+        getSiteName: getSiteName,
+        getId: getId,
+        VideoStorage: VideoStorage,
+        SCHEMA_VERSION: SCHEMA_VERSION,
+    };
+})();
--- youtube.js
+++ youtube.js
@@ -17,106 +17,98 @@ You should have received a copy of the GNU General Public License
 along with resumeLater. If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*global exports*/
 'use strict';
 
-/*
-const data = require('sdk/self').data;
-const setTimeout = require('sdk/timers').setTimeout;
-const tabs = require('sdk/tabs');
-
-const videos = require('../videos');
-*/
-
-var data = {};
-var setTimeout = {};
-var tabs = {};
-
-var videos = {};
-
-function VideoNotFoundError(message) {
-    this.name = 'VideoNotFoundError';
-    this.message = message || 'No video was found.';
-}
-VideoNotFoundError.prototype = new Error();
-VideoNotFoundError.prototype.constructor = VideoNotFoundError;
-
-function getParameters(url) {
-    var splits = url.split('?');
-
-    if (splits.length === 1) {
-        return {};
-    }
-
-    var queryString = splits[splits.length - 1];
-    splits = queryString.split('&');
-    var params = {};
-    var keyValue;
-    for (var i = 0, l = splits.length; i < l; i++) {
-        keyValue = splits[i].split('=');
-        params[keyValue[0]] = keyValue[1];
-    }
-
-    return params;
-}
-
-function getTime(tab) {
-    var promise = new Promise(function(resolve, reject) {
-
-        var worker = tab.attach({
-            contentScriptFile: [
-                data.url("sites/youtube.js")
-            ]
-        });
-
-        worker.port.on('time', function(time) {
-            resolve(time);
-            worker.destroy();
-        });
-
-        setTimeout(function() {
-            reject(new VideoNotFoundError());
-        }, 1000);
-    });
-
-    return promise;
-}
-
-function getVideo(tab) {
-    const video = {};
-    video.title = tab.title;
-
-    var params = getParameters(tab.url);
-    if (params.v) {
-        video.vid = 'youtube_' + params.v;
-    } else {
-        return Promise.reject(new VideoNotFoundError());
-    }
-    if (params.list) {
-        video.playlistId = params.list;
-    }
-
-    return getTime(tab).then(function success(time) {
-        video.time = time;
-        return video;
-    });
-}
-
-function resumeVideo(video) {
-    var site = 'youtube';
-    var id = videos.getId(video);
-    var url = "https://www.youtube.com/watch?v=" + id +
-        '&t=' + Math.floor(video.time) + 's';
-
-    // Adds the playlist parameter to the URL if the video is part of a
-    // playlist
-    if (video.playlistId) {
-        url += "&list=" + video.playlistId;
-    }
-
-    tabs.open(url);
-}
-
 (function () {
+
+    var data = {};
+    var setTimeout = {};
+    var tabs = {};
+
+    var videos = {};
+
+    function VideoNotFoundError(message) {
+        this.name = 'VideoNotFoundError';
+        this.message = message || 'No video was found.';
+    }
+    VideoNotFoundError.prototype = new Error();
+    VideoNotFoundError.prototype.constructor = VideoNotFoundError;
+
+    function getParameters(url) {
+        var splits = url.split('?');
+
+        if (splits.length === 1) {
+            return {};
+        }
+
+        var queryString = splits[splits.length - 1];
+        splits = queryString.split('&');
+        var params = {};
+        var keyValue;
+        for (var i = 0, l = splits.length; i < l; i++) {
+            keyValue = splits[i].split('=');
+            params[keyValue[0]] = keyValue[1];
+        }
+
+        return params;
+    }
+
+    function getTime(tab) {
+        var promise = new Promise(function(resolve, reject) {
+
+            var worker = tab.attach({
+                contentScriptFile: [
+                    data.url("sites/youtube.js")
+                ]
+            });
+
+            worker.port.on('time', function(time) {
+                resolve(time);
+                worker.destroy();
+            });
+
+            setTimeout(function() {
+                reject(new VideoNotFoundError());
+            }, 1000);
+        });
+
+        return promise;
+    }
+
+    function getVideo(tab) {
+        const video = {};
+        video.title = tab.title;
+
+        var params = getParameters(tab.url);
+        if (params.v) {
+            video.vid = 'youtube_' + params.v;
+        } else {
+            return Promise.reject(new VideoNotFoundError());
+        }
+        if (params.list) {
+            video.playlistId = params.list;
+        }
+
+        return getTime(tab).then(function success(time) {
+            video.time = time;
+            return video;
+        });
+    }
+
+    function resumeVideo(video) {
+        var site = 'youtube';
+        var id = videos.getId(video);
+        var url = "https://www.youtube.com/watch?v=" + id +
+            '&t=' + Math.floor(video.time) + 's';
+
+        // Adds the playlist parameter to the URL if the video is part of a
+        // playlist
+        if (video.playlistId) {
+            url += "&list=" + video.playlistId;
+        }
+
+        tabs.open(url);
+    }
+
     return "Hello, World!";
 })();
